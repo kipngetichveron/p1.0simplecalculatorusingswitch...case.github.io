c2dce315b84811e11941fa742a6514bc69322100
kipngetich veron <150454831+kipngetichveron@users.noreply.github.com>
2023-12-23 22:11:11 +0300

    Add files via upload

---
+++ P1.0 main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+  char op;
+  double first, second;
+  scanf("%f", &first);
+  scanf("%c", &op);
+  scanf("%lf", &second);
+
+  switch (op) {
+    case '+':
+      printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+      break;
+    case '-':
+      printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+      break;
+    case '*':
+      printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+      break;
+    case '/':
+      printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+      break;
+    default:
+      printf("Error! operator is not correct");
+  }
+
+  return 0;
+}
